65303405e0219090ae6c33f9d1617a88b3b3590b
Bunguiu <bnorales@costar.com>
2022-04-08 15:16:00 -0400

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
+const position = [37.533655, -77.435335]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer center={position} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYm5vcmFsZXMiLCJhIjoiY2wxamc1MXFuMDlncjNjcWdjOTNyc2FyeCJ9.RnFL5S2gg46H2WjVXLoWeA"
+        tileSize= {512}
+        zoomOffset= {-1}
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
